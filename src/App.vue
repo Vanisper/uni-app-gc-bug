e16a8387ea71f31f63e6ea7b08bc1743ac02dba1
Vanisper <273266469@qq.com>
2025-04-17 07:04:43 +0800

    fix(bugs): bug 已完全修复

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ onLaunch(() => {})
     <view>uni-types - jsx组件类型提示生效</view>
     <!-- bugs -->
     <swiper>uni-types - 全局组件类型提示不生效</swiper>
-    <CustomGCDemo>自定义 - 全局组件类型提示不生效</CustomGCDemo>
+    <CustomGCDemo demo="aaa">自定义 - 全局组件类型提示不生效</CustomGCDemo>
 </template>
 
 <style></style>
